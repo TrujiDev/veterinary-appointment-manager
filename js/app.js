2e289f2d64fd73d15cfdcc28626a54078c827479
TrujiDev <eetgrisales@gmail.com>
2023-11-18 12:16:03 -0500

    Refactor appointment editing function

--- js/app.js
+++ js/app.js
@@ -23,6 +23,14 @@ class appointments {
 			appointment => appointment.id !== id
 		);
 	}
+
+	editAppointment(updatedAppointment) {
+		this.appointments = this.appointments.map(appointment =>
+			appointment.id === updatedAppointment.id
+				? updatedAppointment
+				: appointment
+		);
+	}
 }
 
 class UI {
@@ -97,7 +105,7 @@ class UI {
 			btnEdit.innerHTML = `Edit <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 				<path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
 				</svg>`;
-			btnEdit.onclick = () => editAppointment(appointment);
+			btnEdit.onclick = () => loadAppointment(appointment);
 
 			divAppointment.appendChild(petParagraph);
 			divAppointment.appendChild(ownerParagraph);
@@ -157,6 +165,8 @@ function newDate(event) {
 	}
 
 	if (edit) {
+		manageAppointments.editAppointment({ ...dateObj });
+
 		form.querySelector('button[type="submit"]').textContent = 'Save changes';
 
 		edit = false;
@@ -194,7 +204,7 @@ function deleteAppointment(id) {
 	ui.showAppointments(manageAppointments);
 }
 
-function editAppointment(appointment) {
+function loadAppointment(appointment) {
 	const { pet, owner, phone, date, hour, symptoms, id } = appointment;
 
 	petInput.value = pet;
